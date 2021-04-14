cdf2dfb3e38630f6e5ec05e3705fed7eae5ab503
Khaos <okh8609@gmail.com>
2021-04-14 14:46:49 +0800

    monitor script finished..

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,81 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
+import json
+import sqlite3
+
 
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello():
-#     return "Hello World10!"
-
 @app.route('/', methods=['GET', 'POST'])
-def parse_request():
+def index():
     print(request.data)
     return "Hello World10!"
+
+
+@app.route('/reg', methods=['POST'])
+def reg():
+    data = json.loads(request.data)
+    print("--------------------###--------------------")
+    print(data['uuid'])
+    print(data['public_url'])
+    print(data['new'])
+    print("--------------------###--------------------")
+
+    conn = sqlite3.connect('iot2.db')
+    cur = conn.cursor()
+    if data['new']:
+        cur.execute("select * from USER where UUID = ?", (data['uuid'],))
+        entry = cur.fetchone()
+        if entry is None:  # No entry found
+            cur.execute("insert into USER (UUID, URL) values (?, ?)",(data['uuid'], data['public_url']))   
+            conn.commit()
+            conn.close()         
+            return jsonify(0) # new uuid reg
+        else:
+            return jsonify(-1) # uuid duplicate (please re-reg)
+    else:
+        cur.execute("update USER set URL = ? where UUID = ?", (data['public_url'], data['uuid']) )  # 門是開的
+        conn.commit()
+        conn.close()    
+        return jsonify(1) # update url
+
+
+@app.route('/uuid/<uuid>', methods=['GET'])
+def check_uuid(uuid):
+    conn = sqlite3.connect('iot2.db')
+    cur = conn.cursor()
+    cur.execute("select * from USER where UUID = ?", (uuid,))
+    entry = cur.fetchone()
+    if entry is None:  # No entry found
+        return jsonify(True) # uuid not exist - 可以使用
+    else:
+        return jsonify(False) # uuid exist - 不能使用
+
+@app.route('/user')
+def print_user_table():
+    conn = sqlite3.connect('iot2.db')
+    cur = conn.cursor()
+    with conn:
+        cur.execute("SELECT * FROM USER")
+        return jsonify(cur.fetchall())
+
+
+# 初始化資料庫
+@app.route('/sql')
+def sql_init():
+    conn = sqlite3.connect('iot2.db')
+    c = conn.cursor()
+    # 建立表格
+    c.execute('''create table if not exists USER
+        (UUID    TEXT    NOT NULL,
+         URL     TEXT    NOT NULL,
+         PRIMARY KEY (UUID));
+        ''')
+    conn.commit()
+    conn.close()
+    return jsonify("OK")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
--- monitor/fswatch/run.sh
+++ monitor/fswatch/run.sh
@@ -1,24 +1,71 @@
 #!/bin/bash
 
+kill $(ps -C 'python3 ./rpi.py' | grep 'py' | awk '{print $1}')
+
+# get UUID
+UUID_FILE_PATH=./UUID.txt
+if [ -f $UUID_FILE_PATH ]; then
+    # 檔案 /path/to/dir/filename 存在
+    echo "File $UUID_FILE_PATH exists."
+    UUID=`cat $UUID_FILE_PATH`
+    if curl -fs https://khaos.tw/uuid/$UUID | grep "true"
+    then
+        echo "error: The UUID.txt file has been tampered with."
+        exit -1
+    fi
+    NEW_USR=false
+else
+    # 檔案 /path/to/dir/filename 不存在
+    echo "File $UUID_FILE_PATH does not exists."
+    # generate UUID
+    UUID=$(uuidgen)
+    until $(curl -sf https://khaos.tw/uuid/$UUID | grep -q "true"); do
+        UUID=$(uuidgen)
+        printf '.'
+        sleep 1
+    done
+    echo $UUID > $UUID_FILE_PATH
+    NEW_USR=true
+fi
+echo $UUID 
+
+# get public domain
+kill $(ps -C 'ngrok' | grep 'ngrok' | awk '{print $1}') 
+ngrok tcp 22 > /dev/null &
+
+url="http://localhost:4040/api/tunnels"
+until $(curl -sf $url | grep -q "public_url"); do
+    printf '.'
+    sleep 1
+done
+PUB_URL="$(curl -sf $url | jq ".tunnels[0].public_url")"
+echo $PUB_URL
+
+# reg to backend
+POST_DATA="{ \"uuid\": \"$UUID\", \"public_url\": $PUB_URL, \"new\": $NEW_USR }"
+echo $POST_DATA
+
+curl -i \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-X POST --data "$POST_DATA" "https://khaos.tw/reg"
+
 # 可以用 python3 -m pyinotify -v /tmp 去看到底發生哪些mask
 # 如：maskname=IN_MODIFY 或 maskname=IN_MOVED_TO|IN_ISDIR ...
 # 然後到 fswatch 的 doc 取看看對應的 Event Flags
-
-
 fswatch -0  --event Created --event MovedTo --event Updated --event Removed /home/kh/iot_project2_rpi | while read -d "" event; do
-    
     echo "File [ ${event} ] has changed."
     
     cp /home/kh/iot_project2_rpi/ok.py ./rpi_.py
     # cp /home/kh/iot_project2_rpi/rpi.py ./rpi_.py
     pylint3 ./rpi_.py
     EXE_RESULT=$?
-    #  1 fatal message issued	 
-    #  2 error message issued	 
+    #  1 fatal message issued
+    #  2 error message issued
     # 32 usage error
-    #  0 no error	 
-    #  4 warning message issued	 
-    #  8 refactor message issued	 
+    #  0 no error
+    #  4 warning message issued
+    #  8 refactor message issued
     # 16 convention message issued
     echo "### EXE_RESULT=$EXE_RESULT"
     if [ $EXE_RESULT -eq 1   -o   $EXE_RESULT -eq 2   -o   $EXE_RESULT -eq 32   -o   $EXE_RESULT -eq 22   -o   $EXE_RESULT -eq 18 ];
@@ -26,9 +73,8 @@ fswatch -0  --event Created --event MovedTo --event Updated --event Removed /hom
         echo "### error.."
     else
         echo "### ok~"
-        kill $(ps -C 'python3 ./rpi.py' | grep 'py' | awk '{print $1}') # kill all previous process
         cp ./rpi_.py ./rpi.py
+        kill $(ps -C 'python3 ./rpi.py' | grep 'py' | awk '{print $1}') # kill all previous process
         python3 ./rpi.py &
-    fi  
-
+    fi
 done
